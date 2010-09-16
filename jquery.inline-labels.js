1873a208d70a1ece6c61b4fac122f11fb2468b41
jduff <duff.john@gmail.com>
2010-09-16 18:57:38 -0400

    make it a plugin that you can invoke

--- jquery.inline-labels.js
+++ jquery.inline-labels.js
@@ -8,75 +8,39 @@
 *
 * Version: 0.1.0
 */
+
 (function($) {
-  
-  //
-  // Helpers
-  //
-  
-  // is an input blank
-  $.fn.isEmpty = function() {
-    return $(this)[0].value === '';
-  };
+  // define the plugin
+  $.fn.inlineLabel = function(options) {
+    // extend the default options with those provided
+    // extending an empty object prevents overriding of our defaults object
+    var opts = $.extend({}, $.fn.inlineLabel.defaults, options);
 
-  // is an input present
-  $.fn.isPresent = function() {
-    return !$(this).isEmpty();
-  };
-
-  // return element for label
-  $.fn.element = function() {
-    return $('#' + $(this).attr('for'));
-  };
-
-  // return top/left offset for element
-  $.fn.innerOffset = function() {
-    var el = $(this);
-
-    // TODO check for nil?
-    var topOffset = 
-      parseInt(el.css('marginTop')) + 
-      parseInt(el.css('paddingTop')) +
-      parseInt(el.css('borderTopWidth'));
-      
-    // TODO check for nil?
-    var leftOffset = 
-      parseInt(el.css('marginLeft')) + 
-      parseInt(el.css('paddingLeft')) +
-      parseInt(el.css('borderLeftWidth'));
-
-    return { top: topOffset, left: leftOffset };
-  };
-  
-  // setup
-  $(function() {
-    var labels = $('label.inline');
-    
     // set as supported
-    labels.addClass('supported')
-    
+    this.addClass('supported')
+
     // delegate mousedown to input
     .mousedown(function(e) {
-      $(this).element().focus();
+      $.fn.inlineLabel.element.call($(this)).focus();
       e.preventDefault();
     })
-    
+
     .each(function() {
       var label = $(this);
-      var el = label.element();
-      
+      var el = $.fn.inlineLabel.element.call($(this));
+
       // calc offset
       var leftOffset = el[0].tagName == "TEXTAREA" ? 0 : 2;
-      
+
       // adopt styling of inputs
       label.css({
-        fontSize: el.css('fontSize'), 
+        fontSize: el.css('fontSize'),
         fontFamily: el.css('fontFamily'),
         fontWeight: el.css('fontWeight'),
         lineHeight: el.css('lineHeight'),
         letterSpacing: el.css('letterSpacing'),
-        top: el.position().top + el.innerOffset().top,
-        left: el.position().left + el.innerOffset().left + leftOffset,
+        top: el.position().top + innerOffset(el).top,
+        left: el.position().left + innerOffset(el).left + leftOffset,
         width: el.width() - leftOffset
       });
 
@@ -84,12 +48,12 @@
       el.addClass('field_with_inline_label').data('inline.label', label);
 
       // show input if empty
-      if (el.isEmpty()) label.addClass("empty");
+      if (isEmpty(el)) label.addClass("empty");
     });
 
     // elm behaviours
     $(".field_with_inline_label")
-    
+
     // focus behaviours
     .focus(function() {
       var el = $(this);
@@ -105,11 +69,11 @@
 
       // set timer
       el.data('inline.timer', window.setInterval(function() {
-        if (el.isPresent()) label.removeClass('empty');
+        if (isPresent(el)) label.removeClass('empty');
       }, 25));
-    }) 
-    
-    // blur behaviours 
+    })
+
+    // blur behaviours
     .blur(function() {
       var el = $(this);
       var label = el.data('inline.label');
@@ -123,11 +87,49 @@
       el.data('inline.timer', null);
 
       // show label on blur if field empty
-      if (el.isEmpty()) label.addClass("empty");
+      if (isEmpty(el)) label.addClass("empty");
     });
 
     // ready! show labels
-    $('label.inline').show();
-  });
+    // return this so that it can be chained
+    return this.show();
+  };
 
+  // private function that can only be used within the plugin
+  function innerOffset(obj) {
+    var el = $(obj);
+
+    // TODO check for nil?
+    var topOffset =
+      parseInt(el.css('marginTop')) +
+      parseInt(el.css('paddingTop')) +
+      parseInt(el.css('borderTopWidth'));
+
+    // TODO check for nil?
+    var leftOffset =
+      parseInt(el.css('marginLeft')) +
+      parseInt(el.css('paddingLeft')) +
+      parseInt(el.css('borderLeftWidth'));
+
+    return { top: topOffset, left: leftOffset };
+  };
+
+  function isEmpty(obj) {
+    return $(obj)[0].value === '';
+  };
+
+  // is an input present
+  function isPresent(obj) {
+    return !isEmpty(obj);
+  };
+
+  // public helper function that can be overridden by the user
+  $.fn.inlineLabel.element = function() {
+    return $('#' + this.attr('for'));
+  };
+
+  // plugin defaults
+  $.fn.inlineLabel.defaults = {
+  }; // none yet
 })(jQuery);
+
