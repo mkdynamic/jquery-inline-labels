13ca1636fbe8c4929082583b3b10bfcc0790207a
jduff <duff.john@gmail.com>
2010-09-16 20:21:44 -0400

    remove the css file, doesn't seem worth it to me, make it work with cut and paste

--- jquery.inline-labels.js
+++ jquery.inline-labels.js
@@ -16,11 +16,8 @@
     // extending an empty object prevents overriding of our defaults object
     var opts = $.extend({}, $.fn.inlineLabel.defaults, options);
 
-    // set as supported
-    this.addClass('supported')
-
     // delegate mousedown to input
-    .mousedown(function(e) {
+    this.mousedown(function(e) {
       $.fn.inlineLabel.element.call($(this)).focus();
       e.preventDefault();
     })
@@ -32,23 +29,40 @@
       // calc offset
       var leftOffset = el[0].tagName == "TEXTAREA" ? 0 : 2;
 
-      // adopt styling of inputs
       label.css({
-        fontSize: el.css('fontSize'),
-        fontFamily: el.css('fontFamily'),
-        fontWeight: el.css('fontWeight'),
-        lineHeight: el.css('lineHeight'),
-        letterSpacing: el.css('letterSpacing'),
-        top: el.position().top + innerOffset(el).top,
-        left: el.position().left + innerOffset(el).left + leftOffset,
-        width: el.width() - leftOffset
+        // reset some styles
+        position:       "absolute",
+        cursor:         "text",
+        display:        "none",
+        overflow:       "hidden",
+        padding:        0,
+        margin:         0,
+        border:         0,
+        //and some opacity
+        opacity:        0.75
+      })
+      .css({
+        // adopt styling of inputs
+        fontSize:       el.css('fontSize'),
+        fontFamily:     el.css('fontFamily'),
+        fontWeight:     el.css('fontWeight'),
+        lineHeight:     el.css('lineHeight'),
+        letterSpacing:  el.css('letterSpacing'),
+        top:            el.position().top + innerOffset(el).top,
+        left:           el.position().left + innerOffset(el).left + leftOffset,
+        width:          el.width() - leftOffset
       });
 
       // set input as having inline label
       el.addClass('field_with_inline_label').data('inline.label', label);
 
-      // show input if empty
-      if (isEmpty(el)) label.addClass("empty");
+      if (isEmpty(el)) {
+        if(label.is("inline-focus"))
+          label.css({opacity:opts.focus_opacity});
+        else
+          label.css({opacity:opts.opacity});
+        label.show();
+      }
     });
 
     // elm behaviours
@@ -60,39 +74,43 @@
       var label = el.data('inline.label');
 
       // focus label
-      label.addClass("focus");
+      label.addClass("inline-focus");
 
-      // clear existing timer (maybe don't need this?)
-      var timer = el.data('inline.timer');
-      window.clearInterval(timer);
-      el.data('inline.timer', null);
-
-      // set timer
-      el.data('inline.timer', window.setInterval(function() {
-        if (isPresent(el)) label.removeClass('empty');
-      }, 25));
-    })
-
-    // blur behaviours
-    .blur(function() {
+      if(isEmpty($(this))){
+        label.css({opacity:opts.focus_opacity});
+        $(this).data('inline.label').show();
+      } else {
+        $(this).data('inline.label').hide();
+      }
+    }).blur(function() {
       var el = $(this);
       var label = el.data('inline.label');
 
       // unfocus label
-      label.removeClass("focus");
+      label.removeClass("inline-focus");
 
-      // cancel timer
-      var timer = el.data('inline.timer');
-      window.clearInterval(timer);
-      el.data('inline.timer', null);
+      if(isEmpty($(this))){
+        label.css({opacity:opts.opacity});
+        $(this).data('inline.label').show();
+      } else {
+        $(this).data('inline.label').hide();
+      }
 
-      // show label on blur if field empty
-      if (isEmpty(el)) label.addClass("empty");
+    })
+
+    // input is fired when typing, pasting, cutting
+    .bind('input', function() {
+      if(isEmpty($(this))){
+        label.css({opacity:opts.opacity});
+        $(this).data('inline.label').show();
+      } else {
+        $(this).data('inline.label').hide();
+      }
     });
 
     // ready! show labels
     // return this so that it can be chained
-    return this.show();
+    return this;
   };
 
   // private function that can only be used within the plugin
@@ -115,7 +133,7 @@
   };
 
   function isEmpty(obj) {
-    return $(obj)[0].value === '';
+    return obj[0].value === '';
   };
 
   // is an input present
@@ -130,6 +148,8 @@
 
   // plugin defaults
   $.fn.inlineLabel.defaults = {
+    opacity: 0.75,
+    focus_opacity: 0.25
   }; // none yet
 })(jQuery);
 
